9893ccda18525d2690f9036a4f8c3e014aebbf11
turbogubbu <131949126+turbogubbu@users.noreply.github.com>
2024-02-08 22:42:02 +0100

    added some benchmarking functionality to the main loop

--- src/gameboy/gameboy.rs
+++ src/gameboy/gameboy.rs
@@ -8,6 +8,8 @@ use crate::gameboy::memory::Memory;
 use crate::gameboy::ppu::Ppu;
 use crate::util::video::Video;
 
+use core::arch::x86_64::_rdtsc;
+
 use super::joypad_input::JoypadInput;
 
 pub struct Gameboy {
@@ -43,10 +45,35 @@ impl Gameboy {
         let mut last_event_check = Instant::now();
         let mut last_vram_update = Instant::now();
 
+        let mut start: u64 = 0;
+        let mut end: u64 = 0;
+        let mut execute_instruction_time: u64 = 0;
+        let mut handle_interrupt_time: u64 = 0;
+        let mut ppu_time: u64 = 0;
+        let mut event_time: u64 = 0;
+        let mut misc_stuff_time: u64 = 0;
+        let mut sum_time: u64 = 0;
+
         loop {
+            unsafe {
+                start = _rdtsc();
+            }
+
             self.cpu.execute_single_instruction(&mut self.memory);
 
+            unsafe {
+                end = _rdtsc();
+            }
+
+            execute_instruction_time += end - start;
+            start = end;
+
             self.cpu.handle_interrupt(&mut self.memory);
+
+            unsafe {
+                end = _rdtsc();
+            }
+            handle_interrupt_time += end - start;
 
             if self.cpu.loading_boot_image {
                 //self.display.draw_vram_tiles(&self.memory.data);
@@ -64,6 +91,10 @@ impl Gameboy {
                 self.cpu.load_rom_boot_section = false;
             }
 
+            unsafe {
+                start = _rdtsc();
+            }
+
             if self.cpu.draw_line() && self.ppu.get_lcd_ppu_enable(&self.memory) {
                 if self
                     .ppu
@@ -76,10 +107,23 @@ impl Gameboy {
                 }
             }
 
+            unsafe {
+                end = _rdtsc();
+            }
+
+            ppu_time += end - start;
+            start = end;
+
             if last_event_check.elapsed() >= event_interval {
                 self.video.check_events();
                 last_event_check = Instant::now();
             }
+
+            unsafe {
+                end = _rdtsc();
+            }
+            event_time += end - start;
+            start = end;
 
             if last_vram_update.elapsed() >= vram_interval {
                 self.video.draw_vram_tiles(&self.memory.data);
@@ -87,7 +131,34 @@ impl Gameboy {
                 self.memory.print_ie_register();
                 self.memory.print_interrupt_flag_register();
                 last_vram_update = Instant::now();
+
+                println!("Ppu enabled: {}", self.ppu.get_lcd_ppu_enable(&self.memory));
+
+                print!(
+                    "Benchmarking:\n  Instruction execution: {:10} ({}%)\n  Handle interrupt:      {:10} ({}%)\n  Ppu execution:         {:10} ({}%)\n  Event execution:       {:10} ({}%)\n  Misc execution:        {:10} ({}%)\n",
+                    execute_instruction_time,
+                    execute_instruction_time as f64 / sum_time as f64 * 100.0,
+                    handle_interrupt_time,
+                    handle_interrupt_time as f64 / sum_time as f64 * 100.0,
+                    ppu_time,
+                    ppu_time as f64 / sum_time as f64 * 100.0,
+                    event_time,
+                    event_time as f64 / sum_time as f64 * 100.0,
+                    misc_stuff_time,
+                    misc_stuff_time as f64 / sum_time as f64 * 100.0
+                );
             }
+
+            unsafe {
+                end = _rdtsc();
+            }
+            misc_stuff_time += end - start;
+
+            sum_time = execute_instruction_time
+                + handle_interrupt_time
+                + ppu_time
+                + event_time
+                + misc_stuff_time;
         }
     }
 }
