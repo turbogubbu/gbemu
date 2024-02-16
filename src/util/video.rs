4dc7be27e2a5714cfb5681715dbd56c6e8f9236b
turbogubbu <131949126+turbogubbu@users.noreply.github.com>
2024-02-16 11:41:13 +0100

    outsourced sdl stuff

--- src/util/video.rs
+++ src/util/video.rs
@@ -4,16 +4,12 @@ use crate::gameboy::display::{DIMENSIONS, DIMENSIONS_X, DIMENSIONS_Y};
 use crate::gameboy::joypad_input::JoypadInput;
 use crate::gameboy::ppu::Tile;
 
-use std::process::exit;
-
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use sdl2::render::WindowCanvas;
 use sdl2::surface::Surface;
-use sdl2::ttf::Sdl2TtfContext;
-use sdl2::EventPump;
+use sdl2::Sdl;
+use sdl2::VideoSubsystem;
 
 // LCD pixel to real pixel ratio
 const PIXEL: u32 = 2;
@@ -22,29 +18,24 @@ const WIDTH: u32 = 1600;
 const HEIGHT: u32 = 1000;
 
 pub struct Video {
+    video_subsys: VideoSubsystem,
     canvas: WindowCanvas,
-    event_pump: EventPump,
-    _ttf_context: Sdl2TtfContext,
 }
 
 impl Video {
-    pub fn new() -> Video {
-        let sdl_context = sdl2::init().unwrap();
+    pub fn new(sdl: &Sdl) -> Video {
+        let video_subsys = sdl.video().unwrap();
+        let canvas = video_subsys
+            .window("gbemu", WIDTH, HEIGHT)
+            .position_centered()
+            .build()
+            .unwrap()
+            .into_canvas()
+            .build()
+            .unwrap();
         Video {
-            canvas: sdl_context
-                .video()
-                .unwrap()
-                .window("gbemu", WIDTH, HEIGHT)
-                .position_centered()
-                .build()
-                .unwrap()
-                .into_canvas()
-                .build()
-                .unwrap(),
-
-            event_pump: sdl_context.event_pump().unwrap(),
-
-            _ttf_context: sdl2::ttf::init().map_err(|e| e.to_string()).unwrap(),
+            video_subsys,
+            canvas,
         }
     }
 
@@ -151,97 +142,6 @@ impl Video {
         .unwrap();
 
         self.draw_surface(&surface2, 0, 0, 4);
-    }
-
-    pub fn check_events(&mut self, joypad: &mut JoypadInput) {
-        for event in self.event_pump.poll_iter() {
-            match event {
-                Event::Quit { timestamp: _ } => {
-                    println!("Terminate event received!\n");
-                    exit(0);
-                }
-                Event::KeyDown {
-                    timestamp: _,
-                    window_id: _,
-                    keycode,
-                    scancode: _,
-                    keymod: _,
-                    repeat,
-                } => {
-                    if keycode.is_none() || repeat {
-                        continue;
-                    }
-                    match keycode.unwrap() {
-                        Keycode::K => {
-                            joypad.set_a();
-                        }
-                        Keycode::J => {
-                            joypad.set_b();
-                        }
-                        Keycode::W => {
-                            joypad.set_up();
-                        }
-                        Keycode::A => {
-                            joypad.set_left();
-                        }
-                        Keycode::S => {
-                            joypad.set_down();
-                        }
-                        Keycode::D => {
-                            joypad.set_right();
-                        }
-                        Keycode::Return => {
-                            joypad.set_start();
-                        }
-                        Keycode::Escape => {
-                            joypad.set_select();
-                        }
-                        _ => {}
-                    }
-                }
-                Event::KeyUp {
-                    timestamp: _,
-                    window_id: _,
-                    keycode,
-                    scancode: _,
-                    keymod: _,
-                    repeat: _,
-                } => {
-                    if keycode.is_none() {
-                        continue;
-                    }
-
-                    match keycode.unwrap() {
-                        Keycode::K => {
-                            joypad.reset_a();
-                        }
-                        Keycode::J => {
-                            joypad.reset_b();
-                        }
-                        Keycode::W => {
-                            joypad.reset_up();
-                        }
-                        Keycode::A => {
-                            joypad.reset_left();
-                        }
-                        Keycode::S => {
-                            joypad.reset_down();
-                        }
-                        Keycode::D => {
-                            joypad.reset_right();
-                        }
-                        Keycode::Return => {
-                            joypad.reset_start();
-                        }
-                        Keycode::Escape => {
-                            joypad.reset_select();
-                        }
-                        _ => {}
-                    }
-                }
-                _ => {}
-            }
-        }
     }
 
     fn draw_round_button(&mut self, pos_x: i32, pos_y: i32, pressed: bool) {
